d13ffcb10a5e1a3d1b5d4d341a487e1d8facdb03
osama go <oss.sadi@gmail.com>
2026-03-13 09:12:11 +0200

    Refactor window.gd with improved variable names
    Updated variable names and added comments in Arabic for clarity.

--- window.gd
+++ window.gd
@@ -1,23 +1,23 @@
-extends Area2D
+extends Area2D # لا تخاف تقريبا عامل كل حاجة
 
 var fixed = false
 var player_near = false
 var ok = false
-var odb=0
+var fixed widow = 0 # كل ما يصلح شباك = نقطة
 func _process(delta):
-	if player_near and Input.is_action_just_pressed("fix"):
+	if player_near and Input.is_action_just_pressed("fix"):#input "fix" = f(زر)الحرف 
 		fix_window()
-	if Input.is_action_pressed("reset"):
+	if Input.is_action_pressed("reset"):#input "reset =  r (زر)الحرف
 		get_tree().reload_current_scene()
 		pass
-		if odb == 6:
+		if fixed widow == 6:
 			ok = true
 			pass
 		if ok == true:
-			get_tree().change_scene("res://man menio win.tscn")
+			get_tree().change_scene("res://man menio win.tscn") #man menio win.tscn = قاىءمة الفوز
 			pass
 func _on_window_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player"): #  "player" اله غروب اسمه  ="player"ال
 		player_near = true
 
 func _on_window_body_exited(body):
